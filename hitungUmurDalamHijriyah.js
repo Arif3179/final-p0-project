8dddd63f3c06334eb55578552b5f7b5b7d25d266
arif3179 <rahmawan1@gmail.com>
2019-12-22 10:30:12 +0700

    percantik tampilan

--- hitungUmurDalamHijriyah.js
+++ hitungUmurDalamHijriyah.js
@@ -1,18 +1,21 @@
 function hitungUmurDalamHijriyah(){
+// mencari time interval dari dua tanggal yg berbeda menggunakan build in function getTime
+// Setelah didapat time interval dalam milisecond,lalu dibagi dengan hasil perkalian dari 1000*60*60*24 
     var dob = document.getElementById("dob").value;
-    var today = new Date(); 
+    var today = new Date(); // current date,akan terus berubah tiap hari 
     var tanggalHariIni = today.getFullYear()+"-"+(today.getMonth()+1)+"-"+today.getDate();
     var date2 = new Date(tanggalHariIni)
     var date1 = new Date(dob)
     var umurInMiliSecond = date2.getTime() - date1.getTime()
     var umurHari = umurInMiliSecond / (1000*60*60*24)
+
+ //kalkulasi usia berdasar kalender masehi dengan metode terakurat:
     var dd = date1.getDate()
     var mm = date1.getMonth()+1;
     var yyyy = date1.getFullYear();
-    var tahunMasehi = today.getFullYear()-yyyy 
+    var tahunMasehi = today.getFullYear()-yyyy
     var bulanMasehi = (today.getMonth()+1)-mm
-    var hariMasehi = today.getDate() - dd
-    
+    var hariMasehi = today.getDate() - dd   
     if(hariMasehi >= 0 && bulanMasehi >= 0 && tahunMasehi >= 0){
         umurTahunMasehi = tahunMasehi
         umurBulanMasehi = bulanMasehi
@@ -35,13 +38,27 @@ function hitungUmurDalamHijriyah(){
         umurBulanMasehi = bulanMasehi - 1
         umurHariMasehi = Math.floor(30.43750 - dd) + today.getDate()
     }
-
+// asumsi rata-rata 1 tahun hijriyah = 354,36708 hari
+// asumsi rata-rata 1 bulan hijriyah = 29,53059 hari
     var umurTahunHijriyah = Math.floor(umurHari/354.36708)
     var umurBulanHijriyah = Math.floor((umurHari%354.36708) / 29.53059)
     var umurHariHijriyah = Math.floor((umurHari%354.36708) % 29.53059)
     document.getElementById("hariIni").innerHTML = tanggalHariIni
-    document.getElementById("dalamHari").innerHTML = umurHari+" hari"
-    document.getElementById("kalenderMasehi").innerHTML =umurTahunMasehi+" tahun, "+umurBulanMasehi+" bulan, "+umurHariMasehi+" hari"
     document.getElementById("kalenderHijriyah").innerHTML = umurTahunHijriyah+" tahun, "+umurBulanHijriyah+" bulan, "+umurHariHijriyah+" hari"
+    document.getElementById("kalenderMasehi").innerHTML =umurTahunMasehi+" tahun, "+umurBulanMasehi+" bulan, "+umurHariMasehi+" hari"
+    
+ // Tambah titik per seribu agar lebih enak dibaca   
+    var strNumber = umurHari.toString()
+    let arrHasil = []
+    let hitung = 0
+    for(let i=strNumber.length-1;i>=0;i--){
+        arrHasil.push(strNumber[i])
+        hitung++
+        if(hitung % 3 == 0 && hitung != strNumber.length){
+            arrHasil.push(".");
+        }
+    }  
+    var outputDalamHari =  arrHasil.reverse().join("")
+    document.getElementById("dalamHari").innerHTML = outputDalamHari+" hari"
     }
     
